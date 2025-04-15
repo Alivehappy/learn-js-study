bc453cc5c925c61ab15cd7c0234f76c6b5900dc0
Anna <rozhkova1809@gmail.com>
2025-04-15 11:51:47 +0300

    JS

--- 6 глава/6.3-6.5задачи.js
+++ 6 глава/6.3-6.5задачи.js
@@ -51,9 +51,61 @@ console.log(genId()); // "00001"
 console.log(genId()); // 2
 //Напишите функцию createValidator(rules), которая проверяет данные по правилам:
 function createValidator(rule) {
-	let ruleKeys = Object.keys(rule);
-	return ruleKeys.map(elem => elem < ruleKeys[1] && elem > ruleKeys[0]);
+	return function (value) {
+		return value >= rule.min && value <= rule.max;
+	};
 }
 const validate = createValidator({ min: 3, max: 10 });
+
 console.log(validate(5)); // true
 console.log(validate(11)); // false
+/*Создайте функцию createTimer(seconds), которая возвращает объект с методами:
+
+start() — запускает отсчёт,
+
+pause() — останавливает,
+
+getTime() — возвращает оставшееся время.
+   */
+function createTimer(seconds) {
+	let remainingSeconds = seconds;
+	let timerId = null;
+	let isRunning = false; // Работает ли таймер
+	return {
+		start() {
+			if (!isRunning && remainingSeconds > 0) {
+				isRunning = true;
+				timerId = setInterval(() => {
+					remainingSeconds--;
+					if (remainingSeconds <= 0) {
+						clearInterval(timerId);
+						isRunning = false;
+					}
+				}, 1000);
+			}
+		},
+		pause() {
+			if (isRunning) {
+				//// Если таймер работает
+				clearInterval(timerId);
+				isRunning = false; /// Помечаем как остановленный
+			}
+		},
+		getTime() {
+			return remainingSeconds;
+		},
+	};
+}
+//Создайте функцию multiplier(factor), которая возвращает функцию, умножающую аргумент на factor
+
+function multiplier(factor) {
+	return function (n) {
+		let result = factor * n;
+		return result;
+	};
+}
+const double = multiplier(2);
+console.log(double(5)); // 10
+/*Возвращает новую функцию, которая "запоминает" этот factor через замыкание
+
+*/
